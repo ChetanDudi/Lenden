de2afe4ba71ac9238f61207a1e86fb03e1821806
chetan123445 <2022csb1074@iitrpr.ac.in>
2026-06-13 20:13:31 +0530

    kotlin version updated

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.lenden_frontend"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.lenden_frontend"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21 // Razorpay requires API 21+
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
